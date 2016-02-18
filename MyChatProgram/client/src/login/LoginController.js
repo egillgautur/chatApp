b27b7f1bc98f7a19e5c7b88f52f0f72c648d7520
Egill Gautur <egills14@ru.is>
2016-02-18 15:59:44 +0000

    röðun á index fixed

--- MyChatProgram/client/src/login/LoginController.js
+++ MyChatProgram/client/src/login/LoginController.js
@@ -1,23 +1,20 @@
 "use strict";
 
 angular.module('chatApp').controller("LoginController",
-["$scope", "$location", "$rootScope", "$routeParams", 
-function LoginController($scope, $location, $rootScope, $routeParams) {
-    $scope.nickname = "";
+["$scope", "$location", "$rootScope", "$routeParams", "socket",
+function LoginController($scope, $location, $rootScope, $routeParams, socket) {
+    $scope.user = '';
     $scope.login = function() {
-            if($scope.nickname === "") {
-                
-                $location.path("/roomlist");
-               // toastr.error("Einar is a fag")
-            } else { 
-                $scope.errorMessage = "Innskráning mistókst og einar is a fagit";
-                /*socket.emit("adduser", $scope.nickname, function(available) {
+            if($scope.user === '') {
+                $scope.errorMessage = "Please choose a nickname"
+            } else {    
+                socket.emit("adduser", $scope.user, function(available) {
                     if(available) {
-                        $location.path("/roomlist");
+                        $location.path("/roomlist" + $scope.user);
                     } else {
-                        toastr.error("Einar is a superfagit")
+                        $scope.errorMessage = "Einar is a superfagit"
                     }
-                })*/
+                })
             }
     }
  
