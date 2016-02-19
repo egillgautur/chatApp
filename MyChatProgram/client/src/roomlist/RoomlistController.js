36d92ff2a4e5a0d2c598953975fdc9ec4874497b
Egill Gautur <egills14@ru.is>
2016-02-19 13:26:00 +0000

    roomlist virkar, createRoom working

--- MyChatProgram/client/src/roomlist/RoomlistController.js
+++ MyChatProgram/client/src/roomlist/RoomlistController.js
@@ -3,71 +3,40 @@
 angular.module("chatApp").controller("RoomlistController",
 ["$scope", "$location", "$rootScope", "$routeParams", "socket",
 function RoomlistController($scope, $location, $rootScope, $routeParams, socket) {
-    $scope.rooms = [];
-    $scope.roomList = [];
+    $scope.roomlist = [];
+    $scope.roomNames = [];
     $scope.currUser = $routeParams.user;
-    $scope.roomName = '';
-    $scope.successMessage = '';
+
     socket.emit("rooms");
     
-    socket.on("roomList", function (rooms) {
+    /*socket.on("roomList", function (rooms) {
         $scope.roomList = [];
         
         for(var room in rooms) {
             $scope.roomList.push(room);
         }
-    });
+    });*/
     
-    $scope.enterRoom = function(params) {
-        //TO DO
+    $scope.createRoom = function createRoom() {
+		var obj = {room: $scope.roomName, pass: undefined};
+		socket.emit("joinroom", obj, function(accepted, reason) {
+			if (!accepted) {
+				$scope.errorMessage = reason;
+			} else {
+                console.log("sdfs");
+                console.log($scope.roomName);
+				$location.path("room/" + $scope.roomName);
+			}
+		});
+	}
+   
+    var funcRoomlistChanges = function(roomlist) {
+        console.log(roomlist);
+        $scope.roomlist = roomlist;
+        $scope.roomNames = Object.keys(roomlist);
     }
     
-    $scope.createRoom = function() {
-        var newRoom = {
-            room: $scope.name
-        };
-        
-        var roomExists = false;
-        
-        for(var j = 0; j < $scope.roomList.length; j++) {
-            if(newRoom.room === $scope.roomList[j]) {
-                roomExists = true;
-            }
-        }
-        
-        if(!roomExists) {
-            if($scope.name !== '') {
-                $location.path("/roomlist/" + $scope.currUser + "/" + $scope.newRoom.room);
-            } else {
-                $scope.errorMessage = "Please choose a name for room"
-            }
-        } else {
-            $scope.errMessage = "Room" + $scope.name + "already exists"
-        }
-    };
-   /* var funcRoomlistChanges = function(roomList) {
-        console.log(roomList);
-        $scope.$apply(function() {
-            $scope.roomlist = roomList;
-        });
-    }
-    
-    $scope.$watch("name", function(newValue, oldValue) {
-        //senda á server? Eða færa í LoginController?
-        if(newValue) {
-            if(newValue.length > 3) {
-                
-            }
-        }
-        
-    });
-    
-    $scope.name = "EinarFagit";
-  //socket.on("roomlist", function(roomList) {
     socket.on("roomlist", funcRoomlistChanges);
-        
-    });
-    */
     
     $scope.roomlist = [ {
         name: "Einar is a fagit",
@@ -75,5 +44,8 @@ function RoomlistController($scope, $location, $rootScope, $routeParams, socket)
     }, {
         name: "Einar is a superfagit",
         id: 2
+    }, {
+        name: "Einar is a ultramegafagit",
+        id: 3
     }]; //osfrv......
-}])
+}]);
